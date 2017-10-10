804cf4f20817f5c9735fe00e653fb8b9406bd15a
DevThomasS <31936811+DevThomasS@users.noreply.github.com>
2017-10-10 13:28:18 -0500

    Species Definitions

---
+++ Assets/Scripts/Species/Species.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Species : MonoBehaviour
+{
+    private string name;  // name of species - for stretch goal, we will want this to be a string that appears like formal latin names, by D3, number will suffice
+    private int[] location; // in which tiles this species exists.  Assuming tiles can be simplified to their numerical value
+    private int[] genes;  // what genes this species has.  Assuming genes can be simplified to their numerical value
+
+    public void spawn(bool evolve) { // make new object for species in unity - must work with Kyle's procedural generation for D3; for D4, must also allow for new species from evolution
+        if (evolve) { // meaning the new species is made by adding new gene
+
+        }
+        else { // meaning the species spawned is from world creation (starting species)
+
+        }
+    }
+    public string getName() {
+        return name;
+    }
+    public int[] getLocation() {
+        return location;
+    }
+    public int[] getGenes() {
+        return genes;
+    }
+    public void setName(string newName) {
+        name = newName;
+    }
+    public void setLocation(int[] newLocation) {
+        location = newLocation;
+    }
+    public void setGenes(int[] newGenes) {
+        genes = newGenes;
+    }
+}
---
+++ Assets/Scripts/Species/createNewSpecies.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createNewSpecies : MonoBehaviour {
+    private int count;
+	// Use this for initialization
+	void Start () {
+        Species newSpecies = new Species();
+        count++;
+        newSpecies.setName(count.ToString());
+        newSpecies.setLocation(new int[]);
+        newSpecies.setGenes(new int[]);
+        newSpecies.spawn(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
