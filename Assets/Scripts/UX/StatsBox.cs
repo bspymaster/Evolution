dd2e39d47ff0a9be3031f0de4dcbf29dff608b2c
Ian <31904485+Master-Ian@users.noreply.github.com>
2017-10-13 21:47:43 -0500

    sfagddfg

--- Assets/Scripts/UX/StatsBox.cs
+++ Assets/Scripts/UX/StatsBox.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class StatsBox : TileData
 {
-
+    public GameObject windowManagerPrefab;
     public GameObject tileContentsPrefab;
     public GameObject infoPanelPrefab;
     public Text berryFoodPrefab;
@@ -13,6 +13,7 @@ public class StatsBox : TileData
     public GameObject noClickPrefab;
     public GameObject exitButtonPrefab;
     bool toggle = false;
+    bool toggle2 = false;
 
     public void Awake()
     {
@@ -40,30 +41,37 @@ public class StatsBox : TileData
             int numMeat = getNumAmbientMeat();
             string biome = getTileType();
 
+            GameObject windowManager = Instantiate(windowManagerPrefab, new Vector3(0, 0, 5), Quaternion.identity) as GameObject;
+            windowManager.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+            windowManager.gameObject.tag = "WindowManager";
+
             string foodTypes = "Berries: " + numBerries.ToString() + "\nGrass: " + numGrass.ToString() + "\nNuts: " + numNuts.ToString() + "\nLeaves: " + numLeaves.ToString() + "\nMeat: " + numMeat.ToString();
             string biomeType = biome;
 
             Text foods = Instantiate(berryFoodPrefab, new Vector3(-200, -50, -5), Quaternion.identity) as Text;
-            foods.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+            foods.transform.SetParent(GameObject.FindGameObjectWithTag("WindowManager").transform, false);
 
             Text biomeTitle = Instantiate(biomePrefab, new Vector3(0, 125, -5), Quaternion.identity) as Text;
-            biomeTitle.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+            biomeTitle.transform.SetParent(GameObject.FindGameObjectWithTag("WindowManager").transform, false);
 
             GameObject infoPanel = Instantiate(infoPanelPrefab, new Vector3(0, 0, 5), Quaternion.identity) as GameObject;
-            infoPanel.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+            infoPanel.transform.SetParent(GameObject.FindGameObjectWithTag("WindowManager").transform, false);
 
             GameObject noClick = Instantiate(noClickPrefab, new Vector3(500, 300, -1), Quaternion.identity) as GameObject;
             //noClick.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
 
             GameObject exitButton = Instantiate(exitButtonPrefab, new Vector3(335, 150, -5), Quaternion.identity) as GameObject;
-            exitButton.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+            exitButton.transform.SetParent(GameObject.FindGameObjectWithTag("WindowManager").transform, false);
 
             //berryFood = GetComponent<Text>();
             foods.text = foodTypes;
             biomeTitle.text = biomeType;
 
+            toggle = true;
+            return;
+
         }else if(toggle == true){
-            Destroy(foods);
+            
 
         }
     }
