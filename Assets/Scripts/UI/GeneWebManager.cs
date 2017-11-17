6a9cc148854ed225dac8c439c872f8a7a4098412
Ian <31904485+Master-Ian@users.noreply.github.com>
2017-11-17 01:21:02 -0600

    UI Implememntation 1

---
+++ Assets/Scripts/UI/GeneWebManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GeneWebManager : MonoBehaviour {
+
+
+    public void AddNode(int nodeVal)
+    {
+        Global.newGenes.Add(nodeVal);
+    }
+
+    public void RemoveNode(int nodeVal)
+    {
+        Global.removeGenes.Remove(nodeVal);
+    }
+
+    public void DisableButton()
+    {
+        
+        this.GetComponent<Button>().interactable = false;
+    }
+
+    public void ReadStats()
+    {
+        Debug.Log(Global.newGenes);
+    }
+}
+
+
