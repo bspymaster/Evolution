1954fa2bd7ce22f7631ee703573345473a5e35e4
DevThomasS <31936811+DevThomasS@users.noreply.github.com>
2017-11-29 22:06:11 -0600

    Update Species Hotfix

--- Assets/Scripts/Species/UpdateSpecies.cs
+++ Assets/Scripts/Species/UpdateSpecies.cs
@@ -42,7 +42,7 @@ public class UpdateSpecies : MonoBehaviour {
             lctn.Add(new Vector2Int(locX, locY));
             GameObject newSpeciesObject = Instantiate(speciesObject, new Vector2(DIMENSION * locX, DIMENSION * locY), Quaternion.identity);
             speciesScript = newSpeciesObject.GetComponent<Species>();
-            speciesScript.Init(i.ToString(), lctn, gns, new int[4], 0, 0, 0, 0, 0, 0, 0, 0);
+            speciesScript.Init(i.ToString(), i, lctn, gns, new int[4], 0, 0, 0, 0, 0, 0, 0, 0);
             // set parameters
             for (int j = 0; j < 11; j++)
             {
@@ -58,8 +58,12 @@ public class UpdateSpecies : MonoBehaviour {
         locY = rnd.Next(0, 100);
         lctn.Add(new Vector2Int(locX, locY));
         GameObject newPlayerSpeciesObject = Instantiate(playerSpeciesObject, new Vector2(DIMENSION * locX, DIMENSION * locY), Quaternion.identity);
-        speciesScript.Init("0", lctn, gns, new int[4], 0, 0, 0, 0, 0, 0, 0, 0);
+        speciesScript = newPlayerSpeciesObject.GetComponent<Species>();
+        speciesScript.Init("0", 0, lctn, gns, new int[4], 0, 0, 0, 0, 0, 0, 0, 0);
         speciesArray.Add(newPlayerSpeciesObject);
+        Dictionary<string, int> localPlayerSpecies = new Dictionary<string, int>();
+        localPlayerSpecies.Add("0", 10);
+        GameObject.Find("TileList").GetComponent<TileListData>().getTileAtLocation(new Vector2Int(locX, locY)).GetComponent<TileData>().setLocalSpecies(localPlayerSpecies);
     }
 
     /*
@@ -178,24 +182,17 @@ public class UpdateSpecies : MonoBehaviour {
                 print(playerSpeciesObject.GetComponent<Species>().getGenes());
             }
         }
+        List<Vector2Int> occupiedTile = new List<Vector2Int>();
         List<Vector2Int> location = new List<Vector2Int>();
-        int population = 0;
-        for (int i = 0; i < speciesArray.Count; i++)    //  Iterates through all alive species, and increases their population in each tile
-        {
+        //  get each occupied tile
+        for (int i = 0; i < speciesArray.Count; i++)
+        {   //  iterate through species
             location = speciesArray[i].GetComponent<Species>().getLocation();
             for (int j = 0; j < location.Count; j++)
-            {
-                Dictionary<string, int> locationDict = GameObject.Find("TileList").GetComponent<TileListData>().getTileAtLocation(location[j]).GetComponent<TileData>().getLocalSpecies();
-                print("Species: " + speciesArray[i].GetComponent<Species>().getSpeciesName() + " x: " + location[j].x + ", y: " + location[j].y + locationDict[i.ToString()]);
-                population = GameObject.Find("TileList").GetComponent<TileListData>().getTileAtLocation(location[j]).GetComponent<TileData>().getLocalSpecies()[i.ToString()];
-                population += population * speciesArray[i].GetComponent<Species>().getLitterSize();
-                if (population > speciesArray[i].GetComponent<Species>().getMaxPerTile())
+            {   //  iterate through species[i]'s location
+                if (!occupiedTile.Contains(location[j]))
                 {
-                    Overpopulation(speciesArray[i].GetComponent<Species>(), false, location[j]);
-                }
-                else
-                {
-                    GameObject.Find("TileList").GetComponent<TileListData>().getTileAtLocation(location[j]).GetComponent<TileData>().setSpeciesPopulation(i.ToString(), population);
+                    occupiedTile.Add(location[j]);
                 }
             }
             //  get mutation variables from species to figure out chance - right now just random
@@ -203,10 +200,94 @@ public class UpdateSpecies : MonoBehaviour {
             int mutVarTemp = 100;
             if (rnd.Next(1, 101) <= mutVarTemp)
             {
-                Mutate(speciesArray[i].GetComponent<Species>(), (speciesArray[i].GetComponent<Species>().getSpeciesName().Equals("0")));
+                Mutate(speciesArray[i].GetComponent<Species>(), (speciesArray[i].GetComponent<Species>().getSpeciesID() == 0) );
+            }
+        }
+        int population = 0;
+        //  add population to each of the species in each occupiedTile
+        for (int i = 0; i < occupiedTile.Count; i++)
+        {   //  go through each tile and get the dictionary of species inside that tile
+            Dictionary<string, int> localSpecies = new Dictionary<string, int>();
+            localSpecies = GameObject.Find("TileList").GetComponent<TileListData>().getTileAtLocation(occupiedTile[i]).GetComponent<TileData>().getLocalSpecies();
+            foreach (KeyValuePair<string, int> species in localSpecies)
+            {
+                population = species.Value;
+                population += population * speciesArray[i].GetComponent<Species>().getLitterSize();
+                if (population > speciesArray[i].GetComponent<Species>().getMaxPerTile())
+                {
+                    //  set to false bc no implementation of player movement
+                    Overpopulation(speciesArray[i].GetComponent<Species>(), false, occupiedTile[i]);
+                }
+                else
+                {
+                    GameObject.Find("TileList").GetComponent<TileListData>().getTileAtLocation(occupiedTile[i]).GetComponent<TileData>().setSpeciesPopulation(species.Key, population);
+                }
+                print(species.Key);
             }
         }
     }
+
+
+    //    private void Reproduce()
+    //    {
+    //        if (Global.change)
+    //        {
+    //            for (int i = 0; i < Global.newGenes.Count; i++)
+    //            {
+    //                playerSpeciesObject.GetComponent<Species>().evolve(true, Global.newGenes[i]);
+    //                print(playerSpeciesObject.GetComponent<Species>().getGenes());
+    //            }
+    //        }
+    //        List<Vector2Int> location = new List<Vector2Int>();
+    //        int population = 0;
+    //        for (int i = 0; i < speciesArray.Count; i++)    //  Iterates through all alive species, and increases their population in each tile
+    //        {
+    //            location = speciesArray[i].GetComponent<Species>().getLocation();
+    //            for (int j = 0; j < location.Count; j++)
+    //            {
+
+    //                // start testing to get population working
+
+    //                Dictionary<string, int> locationDict = GameObject.Find("TileList").GetComponent<TileListData>().getTileAtLocation(location[j]).GetComponent<TileData>().getLocalSpecies();
+
+    //                List<string> keyList = new List<string>(locationDict.Keys);
+    //                for (int q = 0; q < keyList.Count; q++)
+    //                {
+    //                    print(keyList[q]);
+    //                }
+
+    //                print("Species: " + speciesArray[i].GetComponent<Species>().getSpeciesName());
+    //                print("x: " + location[j].x + ", y: " + location[j].y);
+    //                print("population: " + locationDict[i.ToString()]);
+
+    //                //  end
+
+    //                population = GameObject.Find("TileList").GetComponent<TileListData>().getTileAtLocation(location[j]).GetComponent<TileData>().getLocalSpecies()[i.ToString()];
+    //                population += population * speciesArray[i].GetComponent<Species>().getLitterSize();
+    //                if (population > speciesArray[i].GetComponent<Species>().getMaxPerTile())
+    //                {
+
+    //                    //  set to false bc no implementation of player movement
+
+    //                    Overpopulation(speciesArray[i].GetComponent<Species>(), false, location[j]);
+    //                }
+    //                else
+    //                {
+    //                    GameObject.Find("TileList").GetComponent<TileListData>().getTileAtLocation(location[j]).GetComponent<TileData>().setSpeciesPopulation(i.ToString(), population);
+    //                }
+    //            }
+    //            //  get mutation variables from species to figure out chance - right now just random
+    //            System.Random rnd = new System.Random();
+    //            int mutVarTemp = 100;
+    //            if (rnd.Next(1, 101) <= mutVarTemp)
+    //            {
+    //                Mutate(speciesArray[i].GetComponent<Species>(), (speciesArray[i].GetComponent<Species>().getSpeciesName().Equals("0")));
+    //            }
+    //        }
+
+
+    //}
+
 
     /*
      *  Have the herbivore species in each tile containing species eat tile resources
