6e0cce4e61dcb88f41c04fb9c2f6577670e3be1f
Ian <31904485+Master-Ian@users.noreply.github.com>
2017-11-30 00:03:42 -0600

    flop

---
+++ Assets/Scripts/UI/GeneName.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeneName : MonoBehaviour {
+
+    public GameObject geneNum;
+    int nodeIndex = GeneNum.getNum();
+    
+    //public GameObject webBuild;
+    public Text textPrefab;
+
+    // Use this for initialization
+    void Start() {
+
+        //    Node node = GameObject.Find("Web Builder").GetComponent<buildWeb>().getWeb().getNode(nodeIndex);
+        //    string GeneName = node.getName();
+
+        //    textPrefab.text = GeneName;
+        //      Text gene = Instantiate(textPrefab, new Vector3(0, 0, -5), Quaternion.identity) as Text;
+        StartCoroutine(Example());
+
+    }
+
+    IEnumerator Example() { 
+
+        yield return new WaitForSeconds(0);
+
+        textPrefab.text = GameObject.Find("Web Builder")
+            .GetComponent<buildWeb>()
+            .getWeb()
+            .getNode(nodeIndex)
+            .getName();
+    }
+	
+}
---
+++ Assets/Scripts/UI/GeneNum.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneNum : MonoBehaviour {
+
+    public int geneNum;
+
+public int getNum()
+    {
+        return geneNum;
+    }
+
+}
