13bfd30ea94eb00696d04b527350cf92c6722b96
Ian <31904485+Master-Ian@users.noreply.github.com>
2017-12-01 17:49:14 -0600

    fghfdhkjgszfd

---
+++ Assets/Scripts/UI/SpriteChange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChange : MonoBehaviour {
+
+    public Sprite BlackCircle;
+    public Sprite RedCircle;
+
+    float timer = 1f;
+    float delay = 1f;
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite == BlackCircle)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = RedCircle;
+                timer = delay;
+                return;
+            }
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite == RedCircle)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = BlackCircle;
+                timer = delay;
+                return;
+            }
+
+        }
+    }
+
+}
